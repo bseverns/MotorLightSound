36e62711b3de27856cf35b2f6d048431be10739b
Ben Severns <severns3@gmail.com>
2024-06-16 13:42:45 -0500

    code refined/refactored - functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,50 @@
-#include "Arduino.h"
-#include <AccelStepper.h>
-#include <LightStrip.h>
-#include <StepperMotor.h>
-#include <SystemController.h>
+#include "main.h"
 
-// Pin definitions
-// lane 4
-#define DIR_PIN4 11
-#define STEP_PIN4 12
-#define SWITCH_PIN4 38
+const int N_STEPPERS = 4;
+StepperControl steppers[N_STEPPERS] = {
+    StepperControl(12, 11, 30),
+    StepperControl(14, 13, 32),
+    StepperControl(16, 15, 36),
+    StepperControl(18, 17, 38)
+};
 
-// lane 3
-#define DIR_PIN3 15
-#define STEP_PIN3 16
-#define SWITCH_PIN3 36
-
-// lane 2
-#define DIR_PIN2 13
-#define STEP_PIN2 14
-#define SWITCH_PIN2 32
-
-// lane 1
-#define DIR_PIN1 17
-#define STEP_PIN1 18
-#define SWITCH_PIN1 30
-
-#define BUTTON_PIN 2
-
-StepperMotor motor1(STEP_PIN1, DIR_PIN1, SWITCH_PIN1);
-StepperMotor motor2(STEP_PIN2, DIR_PIN2, SWITCH_PIN2);
-StepperMotor motor3(STEP_PIN3, DIR_PIN3, SWITCH_PIN3);
-StepperMotor motor4(STEP_PIN4, DIR_PIN4, SWITCH_PIN4);
-
-LightStrip lightStrip;
-SystemController systemController(motor1, motor2, motor3, motor4, &lightStrip, BUTTON_PIN);
+AudioManager audioManager;
+LEDManager ledManager(23, 42);
+int laneIndPins[4] = { 3, 4, 5, 6 };
+SerialManager serialManager(4, laneIndPins);
+GameControl gameControl(steppers, N_STEPPERS, audioManager, ledManager, serialManager);
 
 void setup() {
-  delay(250);
-  Serial.begin(115200);
-  Serial.println("Setup started");
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    Serial6.begin(9600);
+    Serial7.begin(9600);
+    Serial8.begin(9600);
 
-  // call begin to do the initialization of the system here
-  systemController.begin();
-  Serial.println("Setup completed");
+    gameControl.initialize();
 }
 
 void loop() {
-    systemController.update();
+    gameControl.checkGameState();
+    gameControl.runSteppers();
+    serialEvent1();
+    serialEvent6();
+    serialEvent7();
+    serialEvent8();
+}
+
+void serialEvent1() {
+    serialManager.processSerialData(Serial1, 0);
+}
+
+void serialEvent6() {
+    serialManager.processSerialData(Serial6, 1);
+}
+
+void serialEvent7() {
+    serialManager.processSerialData(Serial7, 2);
+}
+
+void serialEvent8() {
+    serialManager.processSerialData(Serial8, 3);
 }
